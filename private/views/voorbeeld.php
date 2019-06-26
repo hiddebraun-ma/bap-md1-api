ccbfd953959586c3db6f2722fd3cbd7bdd04fea1
Hidde Braun <hidde.braun@gmail.com>
2019-06-26 11:55:17 +0200

    Postcode API voorbeeld Vraag API key aan docent (helaas maar geldig tot september 2019...)

--- private/views/voorbeeld.php
+++ private/views/voorbeeld.php
@@ -1,27 +1,36 @@
 <div class="container">
-    <h1>Voorbeeld</h1>
-    <p>Vul hier de zoekopdracht in voor een film titel, dit voorbeeld roept de
-        <a href="https://omdbapi.com" target="_blank">OMDBAPI</a> aan</p>
-    <form action="<?php echo url( '/voorbeeld/zoeken' ) ?>" method="get">
+    <h1>Postcode API voorbeeld</h1>
+    <p>Vul postcode en huisnummer in</p>
+    <form action="<?php echo url('/voorbeeld/zoeken') ?>" method="get">
         <div class="form-group">
-            <label for="search">Zoek naar</label>
-            <input type="text" name="zoekopdracht" class="form-control"/>
+            <label for="search">Postcode</label>
+            <input type="text" name="postcode" class="form-control"/>
+        </div>
+        <div class="form-group">
+            <label for="search">Huisnummer</label>
+            <input type="text" name="huisnummer" class="form-control"/>
         </div>
         <div class="form-action">
             <button type="submit" class="btn btn-primary">Zoeken</button>
         </div>
     </form>
-	<?php if ( isset( $data ) ): ?>
-        <p>Er zijn <?php echo $data['totalResults'] ?> resultaten gevonden...</p>
+
+    <?php if (isset($data)): ?>
+        <hr>
         <div class="row">
-			<?php foreach ( $data['Search'] as $result ): ?>
-                <div class="col-12 col-sm-6 col-md-4">
-                    <h4><?php echo $result['Title'] ?> (<?php echo $result['Year'] ?>)</h4>
-                    <img src="<?php echo $result['Poster'] ?>" alt="" class="img-fluid"/>
-                    <p><a href="https://www.imdb.com/title/<?php echo $result['imdbID']?>" target="_blank">Bekijk op IMDB</a></p>
+            <?php foreach ($data['_embedded']['addresses'] as $address): ?>
+                <div class="col-12 col-sm-6 col-md4">
+                    <h4><?php echo $address['street'] ?> <?php echo $address['number'] ?><?php if(isset($address['addition'])):?>-<?php echo $address['addition'];?><?php endif;?></h4>
+                    <p>
+                        <?php echo $address['postcode'] ?> <?php echo $address['city']['label'] ?>
+                        (<?php echo $address['province']['label'] ?>)
+                    </p>
+                    <p><a href="http://www.google.com/maps/place/<?php echo $address['geo']['center']['wgs84']['coordinates'][1]?>,<?php echo $address['geo']['center']['wgs84']['coordinates'][0]?>" class="btn btn-primary" target="_blank">Check op Google Maps</a></p>
                 </div>
-			<?php endforeach; ?>
+            <?php endforeach; ?>
+
         </div>
-	<?php endif; ?>
+    <?php endif; ?>
+
 </div>
 
