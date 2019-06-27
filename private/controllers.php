d89b0d454fb826bf588a341e786749fb9e873a0b
Hidde Braun <hidde.braun@gmail.com>
2019-06-27 10:07:45 +0200

    Iets meer comments toegevoegd

--- private/controllers.php
+++ private/controllers.php
@@ -21,10 +21,11 @@ function voorbeeld_zoeken() {
 	// IN het config.php heb ik al de basis url ingesteld, deze gebruik ik, en ik plak hier de juiste service en parameters achter
 	$url          = $CONFIG['API_ENDPOINT'] . '/?apikey='.$CONFIG['API_KEY'].'&s=' . urlencode($zoekopdracht);
 
-	//Als mijn zoekdopracht "Avengers" is, wordt de url dus: https://omdbapi.com/apikey=
+	//Als mijn zoekdopracht "Avengers" is, wordt de url dus: https://omdbapi.com/?apikey=d28c89b3&s=Avengers
 
-	// Hier maak ik een HTTP Client aan, deze is automatisch ingeladen
-	// Documentatie  hoe je deze aanroept: http://docs.guzzlephp.org/en/stable/quickstart.html
+	// Hier maak ik een HTTP Client aan, deze is automatisch ingeladen in dit project
+	// Hier lees je meer over hoe je deze HTTP Client kunt gebruiken:  http://docs.guzzlephp.org/en/stable/quickstart.html
+
 	$client   = new GuzzleHttp\Client();
 	$response = $client->request( 'GET', $url,
 		[
@@ -35,7 +36,12 @@ function voorbeeld_zoeken() {
 		]
 	);
 
+	//De JSON zetten we hier om naar een array!
 	$data = json_decode( $response->getBody()->getContents(), true );
+
+	// Hier laden we de view en geven we de data mee aan de view
+    // De view is in dit geval: private/views/voorbeeld.php
+
 	view('voorbeeld', ['data' => $data]);
 
 }
--- private/views/voorbeeld.php
+++ private/views/voorbeeld.php
@@ -1,8 +1,9 @@
 <div class="container">
     <h1>Voorbeeld</h1>
-    <p>Vul hier de zoekopdracht in voor een film titel, dit voorbeeld roept de
-        <a href="https://omdbapi.com" target="_blank">OMDBAPI</a> aan</p>
-    <form action="<?php echo url( '/voorbeeld/zoeken' ) ?>" method="get">
+
+    <p>Vul hier de zoekopdracht in voor een film titel, dit voorbeeld roept de <a href="https://omdbapi.com" target="_blank">OMDBAPI</a> aan</p>
+
+    <form action="<?php echo url('/voorbeeld/zoeken') ?>" method="get">
         <div class="form-group">
             <label for="search">Zoek naar</label>
             <input type="text" name="zoekopdracht" class="form-control"/>
@@ -11,17 +12,22 @@
             <button type="submit" class="btn btn-primary">Zoeken</button>
         </div>
     </form>
-	<?php if ( isset( $data ) ): ?>
+
+    <?php if (isset($data)): ?>
+
         <p>Er zijn <?php echo $data['totalResults'] ?> resultaten gevonden...</p>
+
         <div class="row">
-			<?php foreach ( $data['Search'] as $result ): ?>
+            <?php foreach ($data['Search'] as $result): ?>
                 <div class="col-12 col-sm-6 col-md-4">
                     <h4><?php echo $result['Title'] ?> (<?php echo $result['Year'] ?>)</h4>
                     <img src="<?php echo $result['Poster'] ?>" alt="" class="img-fluid"/>
-                    <p><a href="https://www.imdb.com/title/<?php echo $result['imdbID']?>" target="_blank">Bekijk op IMDB</a></p>
+                    <p><a href="https://www.imdb.com/title/<?php echo $result['imdbID'] ?>" target="_blank">Bekijk op
+                            IMDB</a></p>
                 </div>
-			<?php endforeach; ?>
+            <?php endforeach; ?>
         </div>
-	<?php endif; ?>
+    <?php endif; ?>
+
 </div>
 
